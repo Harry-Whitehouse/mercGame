827915e640b3f8c081221168c939788076425fc4
Harry-Whitehouse <harry.w1995@gmail.com>
2022-02-21 01:15:39 +0000

    js

---
+++ 12-For Loops/script.js
@@ -0,0 +1,16 @@
+let population = 100;
+// let time = 1;
+
+// while (time <= 10) {
+//     time++;
+//     population *= 1.05;
+
+//     console.log("Year:", time);
+//     console.log("population:", population);
+// }
+
+for (let i = 1; i <= 10; i++) {
+    population *= 1.05;
+    console.log("Year:", i);
+    console.log("population:", population);
+}
